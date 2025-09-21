fd1d7dd8b85ead9da5b97fd349e55d597f5a5e94
minchey <nolam@naver.com>
2025-09-22 00:36:50 +0900

    클라이언트 서버연결

--- src/main/java/com/securechat/network/ChatClient.java
+++ src/main/java/com/securechat/network/ChatClient.java
@@ -1,4 +1,16 @@
 package com.securechat.network;
 
+import java.net.Socket;
+import java.io.IOException;
+
 public class ChatClient {
+    public static void main(String[] args) {
+        try{
+            //서버에 연결
+            Socket socket = new Socket("localhost",9999);
+            System.out.println("서버에 연결됨: " + socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/securechat/network/ChatServer.java
+++ src/main/java/com/securechat/network/ChatServer.java
@@ -9,10 +9,12 @@ import java.lang.Thread;
 public class ChatServer {
     public static void main(String[] args) {
         try {
+            //포트 9999에서 서버열기
             ServerSocket serverSocket = new ServerSocket(9999);
             System.out.println("서버 시작됨 (포트: 9999)");
 
             while (true) {
+                //클라이언트 접속 대기
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("클라이언트 연결됨: " + clientSocket);
             }
