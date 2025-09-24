93b15b0de71a159b33917b1f4df5e5ef2ebf808b
minchey <nolam@naver.com>
2025-09-24 21:35:21 +0900

    closeConnection 메서드 생성

--- src/main/java/com/securechat/network/server/ClientHandler.java
+++ src/main/java/com/securechat/network/server/ClientHandler.java
@@ -1,12 +1,11 @@
 package com.securechat.network.server;
 
-import org.springframework.security.core.parameters.P;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class ClientHandler {
     //필드
@@ -18,11 +17,18 @@ public class ClientHandler {
     //기본 생성자
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
-        try {
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.nickname = "Guest";
+
+        try { //예외처리
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),StandardCharsets.UTF_8) );
             out = new PrintWriter(clientSocket.getOutputStream(), true);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    private void closeConnection() throws IOException{
+        if(in != null) in.close();
+        if(out != null) out.close();
+        if(clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
     }
 }
