80aa5d2ed91658d841f7db82044761160205c380
minchey <nolam@naver.com>
2025-09-27 12:59:03 +0900

    Dispatcher 작성

---
+++ src/main/java/com/securechat/network/server/MsgDispatcher.java
@@ -0,0 +1,23 @@
+package com.securechat.network.server;
+
+import com.securechat.model.MsgFormat;
+
+public class MsgDispatcher {
+    public static void dispatch(MsgFormat msg) {
+        switch (msg.getType()) {
+            case CHAT:
+                ChatService.handle(msg);
+                break;
+            case HISTORY:
+                HistoryService.handle(msg);
+                break;
+            case KEY_EXCHANGE:
+                KeyService.handle(msg);
+                break;
+            case SYSTEM:
+                SystemService.handle(msg);
+                break;
+            default -> System.out.println("❗Unknown type: " + msg.getType());
+        }
+    }
+}
