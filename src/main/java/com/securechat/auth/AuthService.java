c29c61821cc6c44b3c999da30e0cde674e96890c
minchey <nolam@naver.com>
2025-10-01 19:58:27 +0900

    guava라이브러리 적용

--- src/main/java/com/securechat/auth/AuthService.java
+++ src/main/java/com/securechat/auth/AuthService.java
@@ -1,4 +1,13 @@
 package com.securechat.auth;
 
+import com.securechat.model.MsgFormat;
+import com.google.common.hash.Hashing;
 public class AuthService {
+
+    public static void handleSignup(MsgFormat msg){
+        String id = msg.getSender();
+        String pw = msg.getBody();
+    }
 }
+
+
