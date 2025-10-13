4830c177b7e3dfdc3d731a271f682f8067226e0c
minchey <nolam@naver.com>
2025-10-13 23:24:29 +0900

    인증게이트 작성

--- src/main/java/com/securechat/network/server/MsgDispatcher.java
+++ src/main/java/com/securechat/network/server/MsgDispatcher.java
@@ -7,12 +7,25 @@ import com.securechat.network.server.service.HistoryService;
 import com.securechat.auth.AuthService;
 public class MsgDispatcher {
     public static void dispatch(MsgFormat msg, ClientHandler clientHandler) {
+
+        // ✅ 인증 전 차단: CHAT/HISTORY
+        switch (msg.getType()) {
+            case CHAT, HISTORY -> {
+                if (!clientHandler.isAuthenticated()) {
+                    clientHandler.sendMessage("{\"type\":\"SYSTEM\",\"body\":\"AUTH_REQUIRED\"}");
+                    return;
+                }
+            }
+            default -> { /* pass */ }
+        }
+
         switch (msg.getType()) {
             case CHAT:                              //채팅 핸들러
                 ChatService.handle(msg);
                 break;
             case SIGNUP:                            //회원가입 핸들러
                 AuthService.handleSignup(msg);
+                break;
             case HISTORY:                           //히스토리 핸들러
                 HistoryService.handle(msg);
                 break;
